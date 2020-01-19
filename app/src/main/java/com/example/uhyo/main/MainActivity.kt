772a26a5050aa42cb12434a8959c976862ff6de2
sinnchan <sinn67c3z42@icloud.com>
2020-01-19 16:56:01 +0900

    add mainMenuActivityFragment refactor MainActivity

--- app/src/main/java/com/example/uhyo/main/MainActivity.kt
+++ app/src/main/java/com/example/uhyo/main/MainActivity.kt
@@ -9,5 +9,12 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if(savedInstanceState == null){
+            val fragment = MainMenuFragment()
+            val transaction = supportFragmentManager.beginTransaction()
+            transaction.add(R.id.main_InsertFragmentLocation, fragment)
+            transaction.commit()
+        }
     }
 }
