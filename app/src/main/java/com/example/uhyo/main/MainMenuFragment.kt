0e8723fb352154eb4cbdfead18b0d0f5b10ce633
sinnchan <sinn67c3z42@icloud.com>
2020-01-19 19:54:39 +0900

    create MainActivity Transaction

--- app/src/main/java/com/example/uhyo/main/MainMenuFragment.kt
+++ app/src/main/java/com/example/uhyo/main/MainMenuFragment.kt
@@ -4,8 +4,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.example.uhyo.R
+import kotlinx.android.synthetic.*
 
 class MainMenuFragment : Fragment(){
 
@@ -19,6 +22,34 @@ class MainMenuFragment : Fragment(){
         savedInstanceState: Bundle?
     ): View? {
         super.onCreateView(inflater, container, savedInstanceState)
-        return inflater.inflate(R.layout.fragment_main_menu, container, false)
+        val view = inflater.inflate(R.layout.fragment_main_menu, container, false)
+        onClickAction(view)
+        return view
     }
+
+    private fun onClickAction(view: View){
+        val solo: Button = view.findViewById(R.id.mainMenuSoloPlayButton)
+        val multi: Button = view.findViewById(R.id.mainMenu2PlayerButton)
+        val online: Button = view.findViewById(R.id.mainMenuOnlinePlayButton)
+
+        activity?.let {
+            val fragmentManager = it.supportFragmentManager.beginTransaction()
+
+            solo.setOnClickListener{
+                fragmentManager.replace(R.id.main_InsertFragmentLocation, SoloFragment())
+                fragmentManager.commit()
+            }
+            multi.setOnClickListener{
+                Toast.makeText(context, "mada", Toast.LENGTH_LONG).show()
+//                fragmentManager.replace(R.id.main_InsertFragmentLocation, MultiFragment())
+            }
+            online.setOnClickListener{
+                fragmentManager.replace(R.id.main_InsertFragmentLocation, MultiFragment())
+                fragmentManager.commit()
+            }
+        }
+
+
+    }
+
 }
