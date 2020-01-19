00cfd693c808b57a26877aabb269dfac8315e578
sinnchan <sinn67c3z42@icloud.com>
2020-01-19 19:01:09 +0900

    add multiFragment and layout

---
+++ app/src/main/java/com/example/uhyo/main/MultiFragment.kt
@@ -0,0 +1,24 @@
+package com.example.uhyo.main
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.uhyo.R
+
+class MultiFragment : Fragment(){
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        super.onCreateView(inflater, container, savedInstanceState)
+        return inflater.inflate(R.layout.fragment_play_multi, container, false)
+    }
+}
