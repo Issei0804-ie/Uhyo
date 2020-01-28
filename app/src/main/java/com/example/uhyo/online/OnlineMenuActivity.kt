962d3323ddd6dfd80787509632988d50b6382e81
sinnchan <sinn67c3z42@icloud.com>
2020-01-28 23:34:51 +0900

    add Activity and Fragments

---
+++ app/src/main/java/com/example/uhyo/online/OnlineMenuActivity.kt
@@ -0,0 +1,11 @@
+package com.example.uhyo.online
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class OnlineMenuActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
---
+++ app/src/main/java/com/example/uhyo/online/SetNameFrament.kt
@@ -0,0 +1,24 @@
+package com.example.uhyo.online
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.uhyo.R
+
+class SetNameFrament: Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        super.onCreateView(inflater, container, savedInstanceState)
+        return inflater.inflate(R.layout.fragment_online_setname, container, false)
+    }
+}
