c4bc1aa6e3974b2338897097e0acb33422f7bcd4
sinnchan <sinn67c3z42@icloud.com>
2020-06-27 02:08:22 +0900

    ごめんなさい

---
+++ Android/app/src/main/java/com/example/uhyo/presentation/presenter/view/IViewHandler.kt
@@ -0,0 +1,4 @@
+package com.example.uhyo.presentation.presenter.view
+
+interface IViewHandler {
+}
---
+++ Android/app/src/main/java/com/example/uhyo/presentation/util/Page.kt
@@ -0,0 +1,7 @@
+package com.example.uhyo.presentation.util
+
+import com.example.uhyo.R
+
+enum class Page(val LayoutResId: Int) {
+    MainMenu(R.id.root);
+}
